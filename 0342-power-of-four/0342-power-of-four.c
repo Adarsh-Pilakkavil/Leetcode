2931480a43968304cd556727dc01505a85fdb19a
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-11 15:44:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.87%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,21 @@
+bool isPowerOfFour(int n) 
+{
+    if (n==0)
+    {
+        return false;
+    }
+    while (n!=1)
+    {
+        if(n%4==0)
+        {
+            n=n/4;
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
