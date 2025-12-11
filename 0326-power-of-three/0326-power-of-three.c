c10abe531bac77f49abca7ed9a3432209edba934
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-11 15:45:26 +0530

    Time: 3 ms (56.83%), Space: 8.5 MB (57.1%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.c
@@ -0,0 +1,21 @@
+bool isPowerOfThree(int n) 
+{
+    if (n==0)
+    {
+        return false;
+    }
+    while (n!=1)
+    {
+        if(n%3==0)
+        {
+            n=n/3;
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
