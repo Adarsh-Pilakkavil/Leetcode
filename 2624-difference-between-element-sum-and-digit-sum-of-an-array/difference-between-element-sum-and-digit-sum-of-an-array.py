2a09063b2f109dcade34906df9f2ed03c58ee07f
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-11 17:13:14 +0530

    Delete 2624-difference-between-element-sum-and-digit-sum-of-an-array directory

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def differenceOfSum(self, nums: List[int]) -> int:
-        ans= 0
-        for i in nums:
-            ans+= i
-            while i:
-                ans-=i%10
-                i//=10
-        return ans
