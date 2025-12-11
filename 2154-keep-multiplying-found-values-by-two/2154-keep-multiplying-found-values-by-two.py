da4f7bdae46eb9a239eebd070cd19d00147a7f20
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-11 16:28:51 +0530

    Time: 0 ms (100%), Space: 17.8 MB (87.01%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -4,8 +4,10 @@ class Solution:
             if nums[0]==original:
                 return original*2
         i=0
-        for i in range(len(nums)):
-            if nums[i]==original:
-                original*=2
-                i=-1
+        while i!=len(nums)-1:
+            for i in range(len(nums)):
+                if nums[i]==original:
+                    original*=2
+                    i=-1
+                    break
         return original
