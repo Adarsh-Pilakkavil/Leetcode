6571b6a5b7ebf99bf1c67bab39a202a0b58ce6a7
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-11 15:45:14 +0530

    Time: 0 ms (100%), Space: 13.1 MB (93.78%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        l=[]
+        for i in range(1,n+1,1):
+            if i%3==0 and i%5==0:
+                l.append("FizzBuzz")
+                continue
+            if i%3==0:
+                l.append("Fizz")
+                continue
+            if i%5==0:
+                l.append("Buzz")
+                continue
+            l.append(str(i))
+        return l
+
+        
