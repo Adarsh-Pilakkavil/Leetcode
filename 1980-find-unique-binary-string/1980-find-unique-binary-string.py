aca4dd1e43783b596c4ef01dab125a1d2d3ef289
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-13 01:00:11 +0530

    Time: 0 ms (100%), Space: 17.9 MB (54.23%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -2,12 +2,17 @@ class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         t=len(nums[0])
         l='0'
+        m=False
         def bt(res,k):
             nonlocal t
             nonlocal l
+            nonlocal m
+            if m:
+                return
             if k==0:
                 if res not in nums and res!="0"*t:
                     l=res
+                    m=True
                 return
             bt(res+"0",k-1)
             bt(res+"1",k-1)
