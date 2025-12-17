91e0e2cfc5f460687b513c6f1b09c6529043f12f
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2025-12-18 00:11:01 +0530

    Time: 0 ms (100%), Space: 18.4 MB (20.53%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -4,7 +4,6 @@ class Solution:
         c=0
         for i in range(n):
             c+=mat[i][i]+mat[i][n-i-1]
-        if n%2==1:
-            return c-mat[n//2][n//2]
-        else:
-            return c
+        if n%2:
+            c-=mat[n//2][n//2]
+        return c
