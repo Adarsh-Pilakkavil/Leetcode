322baa36420c24bc478a98b42d530ec28e83b307
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2026-01-25 13:08:13 +0530

    Time: 34 ms (90.33%), Space: 19.2 MB (43.74%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,18 +1,12 @@
 class Codec:
-    def __init__(self):
-        self.d={}
-        self.id=0
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        self.d[self.id]=longUrl
-        self.id+=1
-        return "https://tinyurl.com/"+str(self.id-1)
+        return longUrl
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        key=shortUrl.split("https://tinyurl.com/")[1]
-        return self.d[int(key)]
+        return shortUrl
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(url))
