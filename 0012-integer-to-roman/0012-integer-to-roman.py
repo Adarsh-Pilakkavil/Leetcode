cd4160822a0a49252146f2d0506e6fb85033bc84
Adarsh Pilakkavil <adarshpilakkavil@gmail.com>
2026-06-19 21:28:36 +0530

    Time: 7 ms (40.7%), Space: 19.4 MB (26.09%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,41 +1,13 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        t=""
-        t+="M"*(num//1000)
-        num=num%1000
-        if num>=900:
-            t+="CM"
-            num-=900
-        if num>=500:
-            t+="D"
-            num-=500
-        if num>=400:
-            t+="CD"
-            num-=400
-        if num>=100:
-            t+="C"*(num//100)
-            num=num%100
-        if num>=90:
-            t+="XC"
-            num-=90
-        if num>=50:
-            t+="L"
-            num-=50
-        if num>=40:
-            t+="XL"
-            num-=40
-        if num>=10:
-            t+="X"*(num//10)
-            num=num%10
-        if num==9:
-            t+="IX"
-            num-=9
-        if num>=5:
-            t+="V"
-            num-=5
-        if num==4:
-            t+="IV"
-            num-=4
-        if num>=1:
-            t+="I"*num
-        return t
+        values=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        roman=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+        i=0
+        res=''
+        while num>0:
+            if num>=values[i]:
+                res+=roman[i]
+                num-=values[i]
+            else:
+                i+=1
+        return res
